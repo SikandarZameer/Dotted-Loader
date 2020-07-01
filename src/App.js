93f35dfd2294f9c8b94bcbd75fe86750a5d1e58a
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-07-01 23:15:50 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Loader from "./Loader";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Loader />
+    </div>
+  );
+}
---
+++ src/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css";
+
+export default () => {
+  return (
+    <div id="loader">
+      <div class="ball" />
+      <div class="ball" />
+      <div class="ball" />
+    </div>
+  );
+};
