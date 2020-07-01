72886a2d08cf6260b1a872109f9bab3291b2f33f
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-07-01 23:18:16 +0500

    sdasda

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -4,9 +4,9 @@ import "./Loader.css";
 export default () => {
   return (
     <div id="loader">
-      <div class="ball" />
-      <div class="ball" />
-      <div class="ball" />
+      <div className="ball" />
+      <div className="ball" />
+      <div className="ball" />
     </div>
   );
 };
